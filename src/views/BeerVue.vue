d2450118423aa510e9bd75083376c92290ab5d49
Kévin Oliva <olivakevin64@gmail.com>
2022-04-27 21:22:55 +0200

    Add double slider

--- src/views/BeerVue.vue
+++ src/views/BeerVue.vue
@@ -10,6 +10,13 @@
       :per-page="perPage"
       aria-controls="my-table"
     ></b-pagination>
+    EBC entre ... et ...
+    <div class='range-slider'>
+      <input type="range" min=1 max=140 step="1" v-model="minEBC">
+      <input type="number" min=1 max=140 step="1" v-model="minEBC">
+      <input type="range" min=1 max=140 step="1" v-model="maxEBC">
+      <input type="number" min=1 max=140 step="1" v-model="maxEBC">
+  </div>
     <p class="mt-3">Page: {{ currentPage }}</p>
     <div v-for="beer of beers" :key="beer.name" :per-page="perPage" :current-page="currentPage">
       <div class="container-fluid">
@@ -17,6 +24,7 @@
           <div class="p-3 fw-bold">{{beer.name}}</div>
           <td>{{beer.description}}</td>
           <td><b>Température de fermentation :</b> {{beer.method.fermentation.temp.value}} °C</td>
+          <td><b>EBC :</b> {{beer.ebc}}</td>
           <img :src=beer.image_url width="50px">
         </div>
       </div>
@@ -36,7 +44,9 @@ export default {
       currentPage: 1,
       searchQuery: '',
       beers: [],
-      nbBeers: 325
+      nbBeers: 325,
+      minEBC: 1,
+      maxEBC: 140,
     }
   },
   mounted () {
@@ -53,25 +63,131 @@ export default {
     currentPage() {
       Vue.axios.get('https://api.punkapi.com/v2/beers?per_page=' + this.perPage + '&page=' + this.currentPage)
       .then((resp)=>{
-        this.beers = resp.data
+        this.beers = resp.data;
       })
     },
     searchQuery() {
-      console.log(this.searchQuery);
+      this.minEBC = 1;
+      this.maxEBC = 140;
       let match = '';
       if (this.searchQuery != '') {
         match = `&beer_name=${this.searchQuery}`
       }
       Vue.axios.get('https://api.punkapi.com/v2/beers?per_page=' + this.perPage + match)
       .then((resp)=>{
-        this.beers = resp.data
+        this.beers = resp.data;
         this.currentPage = 1;
         this.nbBeers = this.beers.length;
         if (this.searchQuery == '') {
           this.nbBeers = 325;
         }
       })
+    },
+    minEBC() {
+      this.updateByTemp();
+    },
+    maxEBC() {
+      this.updateByTemp();
+    }
+  },
+    methods: {
+    slider: function() {
+      if (this.minEBC > this.minEBC) {
+        var tmp = this.maxPH;
+        this.maxPH = this.minEBC;
+        this.minEBC = tmp;
+      }
+    },
+    updateByTemp() {
+      console.log("min :" + this.minEBC);
+      console.log("max :" + this.maxEBC);
+
+      Vue.axios.get('https://api.punkapi.com/v2/beers?per_page=' + this.perPage + '&ebc_gt=' + this.minEBC + '&ebc_lt=' + this.maxEBC)
+      .then((resp)=>{
+        this.beers = resp.data;
+        this.currentPage = 1;
+        this.nbBeers = this.beers.length;
+      })
     }
   }
 }
 </script>
+
+<style scoped>
+.range-slider {
+  width: 300px;
+  margin: auto;
+  text-align: center;
+  position: relative;
+  height: 6em;
+}
+
+.range-slider input[type=range] {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+}
+
+input[type=number] {
+  border: 1px solid #ddd;
+  text-align: center;
+  font-size: 1.6em;
+  -moz-appearance: textfield;
+}
+
+input[type=number]::-webkit-outer-spin-button,
+input[type=number]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+
+input[type=number]:invalid,
+input[type=number]:out-of-range {
+  border: 2px solid #ff6347;
+}
+
+input[type=range] {
+  -webkit-appearance: none;
+  width: 100%;
+}
+
+input[type=range]:focus {
+  outline: none;
+}
+
+input[type=range]:focus::-webkit-slider-runnable-track {
+  background: #2497e3;
+}
+
+input[type=range]:focus::-ms-fill-lower {
+  background: #2497e3;
+}
+
+input[type=range]:focus::-ms-fill-upper {
+  background: #2497e3;
+}
+
+input[type=range]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 5px;
+  cursor: pointer;
+  animate: 0.2s;
+  background: #2497e3;
+  border-radius: 1px;
+  box-shadow: none;
+  border: 0;
+}
+
+input[type=range]::-webkit-slider-thumb {
+  z-index: 2;
+  position: relative;
+  box-shadow: 0px 0px 0px #000;
+  border: 1px solid #2497e3;
+  height: 18px;
+  width: 18px;
+  border-radius: 25px;
+  background: #a1d0ff;
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -7px;
+}
+</style>
